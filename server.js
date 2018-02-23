091a6cd24c7b4c01774baed660d6a0157f0a98ff
Taliesin <tdcochran2@gmail.com>
2018-02-23 14:54:31 -0500

    added mongo, reconfigured sessions

--- server.js
+++ server.js
@@ -1,44 +1,56 @@
+//Set up dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
 const path = require("path");
-const favicon = require("serve-favicon");
 const routes = require("./controllers/controller.js");
-const db = require("./models");
-// const passport = require('passport');
-// const session = require('express-session');
-// const env = require('dotenv').load();
-// const bCrypt = require('bcrypt-nodejs');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
+//Set up middleware
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(favicon(__dirname + '/public/assets/img/favicon.ico'));
-// app.use(session({
-// 	secret: 'chupacabra', 
-// 	resave: false, 
-// 	saveUninitialized:true,
-// 	})); 
-// app.use(require('flash')());
-// app.use(passport.initialize());
-// app.use(passport.session()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+require('./config/passport')(passport);
+require('./app/routes.js')(app,passport,io);
+app.use(session({
+	secret: 'thereOnceWasAManNamedMcGee',
+	cookie: {maxAge: 24* 60* 60 * 1000},
+	unset: 'destroy',
+	resave: true,
+	saveUninitialized: false,
+	stort: store
+}));
 
+//Set up the view engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// require('./routes/auth.js')(app, passport, db, io);
-// require('./config/passport/passport.js')(passport, db.User);
-
-
-
-require('./controllers/controller.js')(app, db, io);
-db.sequelize.sync().then(()=>{
-	server.listen(PORT, () => {
-	  console.log(`Listening on PORT ${PORT}`);
-	});
+//Configure MongoDB server
+const MONGOD_URI = process.env.MONGOD_URI || 'mongodb://localhost:27017/sweet-talk';
+mongoose.Promise = Promise;
+mongoose.connect(MONGOD_URI).then(result => {console.log('Connected to mongo db ' + result.connections[0].name)}).catch(error=> console.log("There was an error: ", error));
+const MongoDBStore = require('connect-mongodb-session')(session);
+const store = new MongoDBStore({uri: MONGODB_URI,
+collection: 'sessions'});
+store.on('error', function(error) {
+	assert.ifError(error);
+	assert.ok(false);
 });
+
+//Set up server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT,	function() {console.log('Server is listening on ' + PORT);
+})
